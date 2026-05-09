0e0c4e6479d6730cae8c41da308fc67ad131345c
Xavier Tondelier <xtondelier@gmail.com>
2026-05-09 13:06:28 +0100

    Create layout for day-scroller

--- src/index.js
+++ src/index.js
@@ -12,12 +12,30 @@ const formHandler = (function () {
     return promise;
   }
 
-  return { getWeatherData };
+  return { getWeatherData, };
+})();
+
+const displayHandler = (function() {
+  const dayScroller = document.querySelector("div.day-scroller > div");
+
+  function displayDataToScroller(dayData) {
+    [...dayScroller.children].forEach(child => child.remove());
+    dayData.forEach(day => {
+      const div = document.createElement("div");
+      div.classList.add("day");
+
+
+      dayScroller.appendChild(div);
+    });
+  }
+
+  return { displayDataToScroller, };
 })();
 
 document.querySelector("button.submit").addEventListener("click", (evt) => {
   evt.preventDefault();
   formHandler.getWeatherData().then((data) => {
     console.log(data);
+    displayHandler.displayDataToScroller(data.forecast);
   });
 });
